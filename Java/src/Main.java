9e3e4ce41f223cf846c6d940d81e8734525ca84f
alicia-basulto <satelliteneptune@gmail.com>
2024-01-18 14:40:04 +0100

    feature/plus66: code schema of the project and first exercise uploaded

---
+++ Java/src/Main.java
@@ -0,0 +1,14 @@
+import plusOne.plusOne66;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        plusOne66 plusOne= new plusOne66();
+        int [] digits = {1,2,3};
+        System.out.println(Arrays.toString(plusOne.plusOne(digits)));
+    }
+}
